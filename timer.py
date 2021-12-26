99c4b41ea0d91a1315ecdc8fcfc27f809e0d331f
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2021-12-27 01:12:32 +0200

    lap counter added

--- timer.py
+++ timer.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import Frame, Tk
 import math
 from datetime import  time
+from tkinter import ttk
 
 # main content frame
 class Main_content(Frame):
@@ -22,7 +23,10 @@ class Main_content(Frame):
 running = False
 # timer at zero
 counter = 0
+# lap data
+lap_count = 1
 
+#lst = []
 # content class
 class Content:
     def __init__(self, root):
@@ -34,7 +38,7 @@ class Content:
 
             def count():
                 if running:
-                    global counter, display
+                    global counter, display, string
 
                     # manage initial delay
                     if counter == 0:
@@ -70,8 +74,14 @@ class Content:
 
         def sw_reset():
             global counter
+            global lap_count
             # reset counter
             counter = 0
+            lap_count = 1
+            lap_text['state']='normal'
+            lap_text.delete("1.0", "end")
+
+            lap_text['state']='disabled'
 
             # if reset when stop
             if running == False:
@@ -80,31 +90,84 @@ class Content:
             # if reset while running
             else:
                 lbl['text']='Starting...'
+
+        def sw_lap():
+            global lst
+            global lap_count
+            # if counter = 0, no lap 
+            if counter == 0:
+                print('stopwatch no running!')
+
+            # if timer is not running but not 0
+            elif running == False and counter != 0:
+                print(string +  ' ' + 'stopwatch is not running!')
+
+            # print lap time
+            else:
+                add_lap()
+                lap_count += 1
+
+
+        def add_lap():
+            #print(lap_count)
+            
+            lap_text['state']="normal"
+            # define text value to get
+            value = ("\t" + str(lap_count) + "\t" + str(display) + '\n')
+            #print(str(value))
+            lap_text.insert(END, value)
+            lap_text['state']="disabled"
+        
+        # stopwatch display labelframe container
+        display_lf = LabelFrame()
+        display_lf.pack()
+
+        # lap info container 
+        lap_lf = LabelFrame(width=10, height=10)
+        lap_lf.pack()
+
+        #lap info titles
+        lap_text_title = Label(lap_lf, text='LAP \t  TIME', font='bold')
+        lap_text_title.pack(side=TOP)#, anchor="w")
+
+        # lap info
+        lap_text = Text(lap_lf, state='disabled', font='bold', width=30, height=10)
+        lap_text.pack(anchor='center')#fill=X)
             
 
         # timer label
-        lbl = Label(text='00:00:00', bg='#ffffff', width=30)
+        lbl = Label(display_lf, text='00:00:00', font='Verdana 30', bg='#ffffff', width=10)
         lbl.pack()
 
-        btn_start = Button(text='Start', command=sw_start)
-        btn_start.pack()
+        # start - stop - reset & lap buttons
+        btn_start = Button(display_lf, text='Start', command=sw_start)
+        btn_start.pack(side='left')
 
-        btn_stop = Button(text='Stop', command=sw_stop)
-        btn_stop.pack()
+        btn_stop = Button(display_lf, text='Stop', command=sw_stop)
+        btn_stop.pack(side='left')
 
-        btn_reset = Button(text='Reset', command=sw_reset)
-        btn_reset.pack()
+        btn_reset = Button(display_lf, text='Reset', command=sw_reset)
+        btn_reset.pack(side='left')
+
+        btn_lap = Button(display_lf, text='Lap', command=sw_lap)
+        btn_lap.pack(side='left')
+
+
+        
+
 
         stopwatch()
 
 
 
+
 def main():
+    # define GUI frame
     root = Tk()
     # title
-    root.title("Simple Python timer")
-    # app size
-    root.geometry('300x200+950+540')
+    root.title("Simple Python Timer")
+    # app screen size
+    root.geometry('300x300+950+540')
     # no resize app
     root.resizable(0,0)
     # show content
